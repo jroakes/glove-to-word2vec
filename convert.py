73a6ed622f97382d1b7ebb9a2450b953c178e57b
polando <cpt.m.qasemi@gmail.com>
2019-07-11 16:49:16 +0200

    Add UTF-8 support Fix UnicodeDecodeError

--- convert.py
+++ convert.py
@@ -16,51 +16,54 @@ import gensim
 
 
 def prepend_line(infile, outfile, line):
-	""" 
-	Function use to prepend lines using bash utilities in Linux. 
-	(source: http://stackoverflow.com/a/10850588/610569)
-	"""
-	with open(infile, 'r') as old:
-		with open(outfile, 'w') as new:
-			new.write(str(line) + "\n")
-			shutil.copyfileobj(old, new)
+    """ 
+    Function use to prepend lines using bash utilities in Linux. 
+    (source: http://stackoverflow.com/a/10850588/610569)
+    """
+    with open(infile, 'r') as old:
+        with open(outfile, 'w') as new:
+            new.write(str(line) + "\n")
+            shutil.copyfileobj(old, new)
+
 
 def prepend_slow(infile, outfile, line):
-	"""
-	Slower way to prepend the line by re-creating the inputfile.
-	"""
-	with open(infile, 'r') as fin:
-		with open(outfile, 'w') as fout:
-			fout.write(line + "\n")
-			for line in fin:
-				fout.write(line)
+    """
+    Slower way to prepend the line by re-creating the inputfile.
+    """
+    with open(infile, 'r', encoding='UTF-8') as fin:
+        with open(outfile, 'w', encoding='UTF-8') as fout:
+            fout.write(line + "\n")
+            for line in fin:
+                fout.write(line)
+
 
 def get_lines(glove_file_name):
     """Return the number of vectors and dimensions in a file in GloVe format."""
-    with smart_open.smart_open(glove_file_name, 'r') as f:
+    with smart_open.smart_open(glove_file_name, 'r', encoding='UTF-8') as f:
         num_lines = sum(1 for line in f)
-    with smart_open.smart_open(glove_file_name, 'r') as f:
+    with smart_open.smart_open(glove_file_name, 'r',  encoding='UTF-8') as f:
         num_dims = len(f.readline().split()) - 1
     return num_lines, num_dims
-	
+
+
 # Input: GloVe Model File
 # More models can be downloaded from http://nlp.stanford.edu/projects/glove/
-glove_file="glove.6B.300d.txt"
+glove_file = "glove.840B.300d.txt"
 
 num_lines, dims = get_lines(glove_file)
 
 # Output: Gensim Model text format.
-gensim_file='glove_model2.txt'
+gensim_file = 'glove_model2.txt'
 gensim_first_line = "{} {}".format(num_lines, dims)
 
 # Prepends the line.
 if platform == "linux" or platform == "linux2":
-	prepend_line(glove_file, gensim_file, gensim_first_line)
+    prepend_line(glove_file, gensim_file, gensim_first_line)
 else:
-	prepend_slow(glove_file, gensim_file, gensim_first_line)
+    prepend_slow(glove_file, gensim_file, gensim_first_line)
 
 # Demo: Loads the newly created glove_model.txt into gensim API.
-model=gensim.models.Word2Vec.load_word2vec_format(gensim_file,binary=False) #GloVe Model
+model = gensim.models.KeyedVectors.load_word2vec_format(gensim_file, binary=False)  # GloVe Model
 
-print model.most_similar(positive=['australia'], topn=10)
-print model.similarity('woman', 'man')
+print (model.most_similar(positive=['australia'], topn=10))
+print (model.similarity('woman', 'man'))
